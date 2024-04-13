1cb0f31fca81890b1e0ab1cd122bf93f5633c12c
Jacob Delott <jldelott@gmail.com>
2024-04-13 16:44:49 -0700

    finished splash page

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,40 +1,40 @@
-import Image from "next/image"
-import MenuItem from "../menu/MenuItem"
-import SectionHeaders from "./SectionHeaders"
+import Image from "next/image";
+import MenuItem from "../menu/MenuItem";
+import SectionHeaders from "./SectionHeaders";
 
-
-export default function HomeMenu(){
-    return (
-        <section className="">
-          <div className="absolute left-0 right-0 w-full 
-          justify-start">
-            <div className="absolute left-0 -top-[70px] text-left -z-10">
-                <Image src={'/sallad1.png'}  width={109} height={189}  
-                 objectFit={'contain'} alt={'sallad'}/>
-            </div>
-            <div className="absolute -top-[100px] right-0 -z-10 ">
-                <Image src={'/sallad2.png'} width={107} height={195}  
-                  alt={'sallad'}/>
-            </div>
-                
-                
-                <div className="text-center mb-4">
-                    <SectionHeaders 
-                        subHeader={'check out the'}
-                        mainHeader={'Menu'}
-                    />
-
-                </div>
-                <div className="grid grid-cols-3 gap-4">
-                    <MenuItem />
-                    <MenuItem />
-                    <MenuItem />
-                    <MenuItem />
-                    <MenuItem />
-                    <MenuItem />
-                </div>
-             </div>
-            
-        </section>
-    )
+export default function HomeMenu() {
+  return (
+    <section className="">
+      <div
+        className="absolute left-0 right-0 w-full 
+          justify-start"
+      >
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
+          <Image
+            src={"/sallad1.png"}
+            width={109}
+            height={189}
+            objectFit={"contain"}
+            alt={"sallad"}
+          />
+        </div>
+        <div className="absolute -top-[100px] right-0 -z-10 ">
+          <Image src={"/sallad2.png"} width={107} height={195} alt={"sallad"} />
+        </div>
+      </div>
+      <div className="text-center">
+        <div className="text-center mb-4">
+          <SectionHeaders subHeader={"check out the"} mainHeader={"Menu"} />
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-import Header from "./components/layout/Header"
-import Hero from "./components/layout/Hero"
-import HomeMenu from "./components/layout/HomeMenu"
-import SectionHeaders from "./components/layout/SectionHeaders"
+import Header from "./components/layout/Header";
+import Hero from "./components/layout/Hero";
+import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -9,12 +9,43 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
-      <section className="text-center my-16" id="about">
-        <SectionHeaders
-          subHeader={'Our story'}
-          mainHeader={'About us'}
-        />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Here at ST Pizza our pizza delivery service, we are dedicated to
+            more than just crafting delicious pies we are committed to
+            delivering an outstanding experience to every customer, every time.
+            With a focus on quality ingredients, mouthwatering flavors, and
+            timely delivery,
+          </p>
+          <p>
+            We aim to be your top choice for satisfying your pizza cravings. Our
+            team consists of passionate pizza enthusiasts who take pride in
+            every pie we create. Whether you are enjoying a classic pepperoni or
+            exploring one of our unique specialties, we strive to exceed your
+            expectations with every bite.
+          </p>
+          <p>
+            At ST Pizza, our mission is simple: to deliver happiness, one slice
+            at a time.
+          </p>
+        </div>
       </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Feed your cravings now"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="818254488">
+            818 625 4488
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All rights reserved{" "}
+      </footer>
     </>
-  )
+  );
 }
