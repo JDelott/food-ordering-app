fd34bdd5a5c14a385c11084b146e87ed80678579
Jacob Delott <jldelott@gmail.com>
2024-04-18 18:46:16 -0700

    added profile logic and styling

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,9 +3,12 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+
 export default function ProfilePage() {
   const session = useSession();
   const [userName, setUserName] = useState("");
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
   const { status } = session;
 
   useEffect(() => {
@@ -16,11 +19,21 @@ export default function ProfilePage() {
 
   async function handleProfileInfoUpdate(ev) {
     ev.preventDefault();
+    setSaved(false);
+    setIsSaving(true);
     const response = await fetch("/api/profile", {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name: userName }),
     });
+    setIsSaving(false);
+    if (response.ok) {
+      setSaved(true);
+    }
+  }
+
+  function handleFileChange(ev) {
+    console.log(ev);
   }
 
   if (status === "loading") {
@@ -37,17 +50,40 @@ export default function ProfilePage() {
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
       <div className="max-w-md mx-auto">
-        <div className="flex gap-4  items-center">
+        {saved && (
+          <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+            Profile saved!
+          </h2>
+        )}
+        {isSaving && (
+          <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+            Saving...
+          </h2>
+        )}
+
+        <div className="flex gap-4 items-center">
           <div>
             <div className="p-2 rounded-lg relative">
               <Image
-                className="rounded-lg w-full h-full mb-1"
+                className="rounded-lg w-20 h-20 mb-1"
                 src={userImage}
                 alt={"avatar"}
-                width={250}
-                height={250}
+                width={450}
+                height={450}
               />
-              <button type="button">Edit</button>
+              <label>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleFileChange}
+                />
+                <span
+                  className="block border border-gray-300 rounded-lg p-2 
+                 text-center cursor-pointer"
+                >
+                  Edit
+                </span>
+              </label>
             </div>
           </div>
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
