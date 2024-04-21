633c98e7ac21f6ee7c1e31258a7f6b9b31bc70d9
Jacob Delott <jldelott@gmail.com>
2024-04-21 12:00:07 -0700

    added aws

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,7 +1,24 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 export async function POST(req) {
   const data = await req.formData();
-  if (data.get("files")) {
-    console.log("we have file", data.get("files"));
+  if (data.get("file")) {
+    const file = data.get("file");
+
+    const s3Client = new S3Client({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+    const ext = file.name.split(".").slice(-1);
+    console.log({ ext });
+    // s3Client.send(
+    //   PutObjectCommand({
+    //     Bucket: "jacob-food-ordering",
+    //     key: "",
+    //   })
+    // );
   }
   return Response.json(true);
 }
