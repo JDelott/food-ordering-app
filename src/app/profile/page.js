8ed2e0377534742e54224d02fee2d3ec723fb9fc
Jacob Delott <jldelott@gmail.com>
2024-04-24 20:51:03 -0700

    finished profile

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -128,43 +128,54 @@ export default function ProfilePage() {
             </div>
           </div>
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <label>First and Last name</label>
             <input
               type="text"
               placeholder="First and Last name"
               value={userName}
               onChange={(ev) => setUserName(ev.target.value)}
             />
+            <label>Email</label>
             <input
               type="text"
               disabled={true}
               placeholder={session.data.user.email}
             />
+            <label>Phone</label>
             <input
               type="tel"
               placeholder="Phone Number"
               value={phone}
               onChange={(ev) => setPhone(ev.target.value)}
             />
+            <label>Street Address</label>
             <input
               type="text"
               placeholder="Street Address"
               value={streetAddress}
               onChange={(ev) => setStreetAddress(ev.target.value)}
             />
-            <div className="flex gap-4">
-              <input
-                type="text"
-                placeholder="Postal code"
-                value={postalCode}
-                onChange={(ev) => setPostalCode(ev.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="City"
-                value={city}
-                onChange={(ev) => setCity(ev.target.value)}
-              />
+            <div className="flex gap-2">
+              <div>
+                <label>Postal code</label>
+                <input
+                  type="text"
+                  placeholder="Postal code"
+                  value={postalCode}
+                  onChange={(ev) => setPostalCode(ev.target.value)}
+                />
+              </div>
+              <div>
+                <label>City</label>
+                <input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={(ev) => setCity(ev.target.value)}
+                />
+              </div>
             </div>
+            <label>Country</label>
             <input
               type="text"
               placeholder="Country"
