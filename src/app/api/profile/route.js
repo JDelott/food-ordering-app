03f3eacbbb270431a9d5f1b84cf5b1732697ca00
Jacob Delott <jldelott@gmail.com>
2024-04-28 20:03:25 -0700

    added loading

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,15 +1,19 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route.js";
 import mongoose from "mongoose";
 import { User } from "@/models/User";
+import { UserInfo } from "@/models/UserInfo";
 import { getServerSession } from "next-auth";
 
 export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
   const data = await req.json();
+  const { name, image, ...otherUserInfo } = data;
   const session = await getServerSession(authOptions);
   const email = session.user.email;
 
-  await User.updateOne({ email }, data);
+  await User.updateOne({ email }, { name, image });
+
+  await UserInfo.findOneAndUpdate({ email }, otherUserInfo, { upsert: true });
 
   return Response.json(true);
 }
@@ -21,5 +25,7 @@ export async function GET() {
   if (!email) {
     return Response.json();
   }
-  return Response.json(await User.findOne({ email }));
+  const user = await User.findOne({ email }).lean();
+  const userInfo = await UserInfo.findOne({ email }).lean();
+  return Response.json({ ...user, ...userInfo });
 }
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,25 +1,35 @@
 "use client";
 import UserTabs from "@/components/layout/UserTabs";
+import { useProfile } from "@/components/UseProfile";
 import { useState, useEffect } from "react";
 
 export default function CategoriesPage() {
-  const [isAdmin, setIsAdmin] = useState(false);
-  useEffect(() => {
-    fetch("/api/profile").then((resoponse) => {
-      Response.json().then((data) => {
-        setIsAdmin(data.admin);
-      });
-    });
-  }, []);
+  const { loading: profileLoading, data: profileData } = useProfile();
 
-  if (!isAdmin) {
+  if (profileLoading) {
+    return "Loading user info";
+  }
+
+  if (!profileData.admin) {
     return "Not an admin";
   }
 
   return (
-    <section className="mt-8 max-w-lg mx-auto">
-      <UserTabs isAdmin={false} />
-      categories
+    <section className="mt-8 max-w-md mx-auto">
+      <UserTabs isAdmin={true} />
+      <form className="mt-8">
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>New category name</label>
+            <input type="text" />
+          </div>
+          <div className="pb-2">
+            <button className="border border-primary" type="Submit">
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
