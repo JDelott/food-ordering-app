040bbc472b714cf5f7467dcba703d2c7e8100457
Jacob Delott <jldelott@gmail.com>
2024-05-24 21:19:37 -0700

    finished order confirmation

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Trash from "@/components/icons/Trash";
 import AddressInputs from "@/components/layout/AddressInputs";
 import { useProfile } from "@/components/UseProfile";
+import CartProduct from "@/components/menu/CartProduct";
 
 export default function CartPage() {
   const { cartProducts, removeCartProduct } = useContext(CartContext);
@@ -90,49 +91,11 @@ export default function CartPage() {
           )}
           {cartProducts.length > 0 &&
             cartProducts.map((product, index) => (
-              <div
-                className="flex gap-4 border-b py-4 mb-4 items-center"
-                key={`${product._id}-${index}`}
-              >
-                <div className="w-24">
-                  <Image
-                    width={240}
-                    height={240}
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </div>
-                <div className="grow">
-                  <h3 className="font-semibold">{product.name}</h3>
-                  {product.size && (
-                    <div className="text-sm">
-                      Size: <span>{product.size.name}</span>
-                    </div>
-                  )}
-                  {product.extras?.length > 0 && (
-                    <div className="text-sm text-gray-500">
-                      Extras:{" "}
-                      {product.extras.map((extra) => (
-                        <div key={`${extra._id}-${product._id}`}>
-                          {extra.name} ${extra.price}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-                <div className="text-lg font-semibold">
-                  ${cartProductPrice(product)}
-                </div>
-                <div className="ml-2">
-                  <button
-                    type="button"
-                    onClick={() => removeCartProduct(product)}
-                    className="p-2"
-                  >
-                    <Trash />
-                  </button>
-                </div>
-              </div>
+              <CartProduct
+                key={index}
+                product={product}
+                onRemove={removeCartProduct}
+              />
             ))}
           <div className="py-2 pr-16 flex justify-end items-center">
             <div className="text-gray-500">
--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -1,9 +1,10 @@
 "use client";
 import SectionHeaders from "@/components/layout/SectionHeaders";
-import { CartContext } from "@/components/AppContext";
+import { CartContext, cartProductPrice } from "@/components/AppContext";
 import { useContext, useEffect, useState } from "react";
 import AddressInputs from "@/components/layout/AddressInputs";
 import { useParams } from "next/navigation";
+import CartProduct from "@/components/menu/CartProduct";
 
 export default function OrderPage() {
   const { clearCart } = useContext(CartContext);
@@ -27,19 +28,44 @@ export default function OrderPage() {
     }
   }, []);
 
+  let subtotal = 0;
+  if (order?.cartProducts){
+    for (const product of order?.cartProducts){
+      subtotal += cartProductPrice(product);
+    }
+
+  }
+
   return (
     <section className="max-w-2xl mx-auto mt-8">
       <div className="text-center">
         <SectionHeaders mainHeader="Your order" />
-        <div className="my-4">
+        <div className="my-4 mb-8">
           <p>Thanks for your order</p>
           <p>We will call you when it is on the way</p>
         </div>
       </div>
       {order && (
         <div className="grid grid-cols-2 gap-16">
-          <div>left - products</div>
-          <div>right - address</div>
+          <div>
+            {order.cartProducts.map(product => (
+              <CartProduct product={product}/>
+            ))}
+            <div className="text-right py-2 text-gray-500 font-bold">
+              Subtotal: 
+              <span className="text-black inline-block w-8">${subtotal}</span>
+              <br/>
+              Delivery: 
+              <span className="text-black inline-block w-8">$5</span>
+              <br/> 
+              Total: 
+              <span className="text-black inline-block w-8">${subtotal + 5}</span>
+              <br/>
+             </div>
+          </div>
+          <div className="bg-gray-100 p-4 rounded-lg">
+            <AddressInputs disabled={true} addressProps={...order} />
+          </div>
         </div>
       )}
     </section>
