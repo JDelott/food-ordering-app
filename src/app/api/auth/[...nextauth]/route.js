d45a1891b3456ba27ffba5da4221c46b75cea5e5
Jacob Delott <jldelott@gmail.com>
2024-05-28 20:57:21 -0700

    deploy

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,3 @@
-// src/app/api/auth/[...nextauth]/route.js
 import clientPromise from "@/libs/mongoConnect";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
@@ -9,7 +8,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 const authOptions = {
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
@@ -48,6 +47,56 @@ const authOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+// // src/app/api/auth/[...nextauth]/route.js
+// import clientPromise from "@/libs/mongoConnect";
+// import bcrypt from "bcrypt";
+// import * as mongoose from "mongoose";
+// import { User } from "@/models/User";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter";
+
+// const authOptions = {
+//   secret: process.env.SECRET,
+//   adapter: MongoDBAdapter(clientPromise),
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
+//       id: "credentials",
+//       credentials: {
+//         username: {
+//           label: "Email",
+//           type: "email",
+//           placeholder: "test@example.com",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials, req) {
+//         const email = credentials?.email;
+//         const password = credentials?.password;
+
+//         await mongoose.connect(process.env.MONGO_URL);
+//         const user = await User.findOne({ email });
+//         const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+//         if (passwordOk) {
+//           return user;
+//         }
+
+//         return null;
+//       },
+//     }),
+//   ],
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
 
 // / import * as mongoose from "mongoose";
 // import { User } from "@/models/User";
